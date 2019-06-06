fb7891e703ea1110bbf8e4ef9b8f0e6ee49d7a2f
angelicamunoz2204 <angelica.benavides@correounivalle.edu.co>
2019-06-06 15:38:49 -0500

    Adicionando ejemplo de archivo de cabecera

--- 2019_05_31/entero.h
+++ 2019_05_31/entero.h
@@ -1,1 +1,10 @@
-#include 
+#ifndef __entero__
+#define __entero__
+
+#define entero int
+#define suma(a,b) a + b
+#define resta(a,b) a - b
+#define multiplicar(a,b) a * b
+#define dividir(a,b) a / b
+
+#endif
